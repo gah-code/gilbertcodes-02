9d4b2cd86ca80bd6ca1ec1ecc07d41ded52575e8
Gilbert Haro <gilbertharocode@gmail.com>
2023-06-17 20:20:12 -0700

    inital commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,43 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  plugins: [],
-}
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@gilbertharo',
+    person: { name: 'gilbert', age: 29 },
+    simpleData: ['item 1', 'item 2'],
+    complexData: [
+      { name: 'gilbert', age: 29 },
+      { name: 'steph', age: 28 },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `rpilh1nqkh8b`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+  ],
+};
