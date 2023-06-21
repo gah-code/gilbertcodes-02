8d2a0006d1de92ae88275445e9c5e7a95a80f4b1
Gilbert Haro <gilbertharocode@gmail.com>
2023-06-20 21:15:16 -0700

    styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,65 +8,55 @@ export default function Home() {
   return (
     <Layout>
       <main className='page'>
-        <section className='about-pages'>
-          <article>
-            <h1>What's up, I'm Gilbert</h1>
-            <p>
-              web developer and this is where I coding things
-              <a href=''> web apps about topics</a>
-            </p>
+        <div className='main'>
+          <div className='container'>
+            <div className='hero-wrapper'>
+              <header className='hero index'>
+                <h1>What's up, I'm Gilbert</h1>
+                <p className='hero-description small width'>
+                  welcome to my space
+                </p>
 
-            <p className='hero-description'>
-              web developer and this is where I coding things Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Quam, nesciunt!
-              <a href=''> web apps about topics</a>
-            </p>
-
-            <button className='rounded-hover-button'>Hover Me</button>
-
-            {/* <Link to='/contact' className='btn'>
-              contact
-            </Link> */}
-          </article>
-
-          <StaticImage
-            src='../assets/images/me.jpg'
-            alt='Person Pouring Salt in Bowl'
-            className='about-imgs'
-            placeholder='blurred'
-          />
-        </section>
-        <section className='about-pagess'>
-          <article>
-            <h2>I'm baby coloring book poke taxidermy</h2>
-            <h3>text</h3>
-            <p>
-              Taxidermy forage glossier letterpress heirloom before they sold
-              out you probably haven't heard of them banh mi biodiesel chia.
-            </p>
-
-            <button className='rounded-hover-button'>Hover Me</button>
-
-            {/* <Link to='/contact' className='btn'>
-              contact
-            </Link> */}
-          </article>
-          <article>
-            <h2>I'm baby coloring book poke taxidermy</h2>
-            <h3>Subheading</h3>
-
-            <p>
-              Taxidermy forage glossier letterpress heirloom before they sold
-              out you probably haven't heard of them banh mi biodiesel chia.
-            </p>
-
-            <button className='rounded-hover-button'>Hover Me</button>
-
-            {/* <Link to='/contact' className='btn'>
-              contact
-            </Link> */}
-          </article>
-        </section>
+                <p className='hero-description'>
+                  I'm web developer and this is where I coding things and where
+                  I talk about
+                  <a href=''> web apps about topics</a>
+                </p>
+                <button className='rounded-hover-button'>Hover Me</button>
+              </header>
+              <p className='hero-decoration'>
+                <StaticImage
+                  src='../assets/images/me.jpg'
+                  alt='Person Pouring Salt in Bowl'
+                  className='hero-image'
+                  placeholder='blurred'
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='article-content'>
+            <div className='post-header medium width'>
+              <h1>Lorem ipsum dolor sit amet.</h1>
+            </div>
+            <div className='segment small'>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Repellat, quos?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis,
+                provident nostrum molestiae iusto asperiores quas omnis nulla!
+                Mollitia optio adipisci beatae exercitationem nemo quae vel quia
+                quo dignissimos amet asperiores sapiente maxime corporis enim
+                corrupti dolorum, voluptatibus quibusdam expedita aliquid fugit
+                repudiandae odit ut minus? Velit voluptatem cupiditate
+                explicabo!
+              </p>
+            </div>
+          </div>
+        </div>
 
         <header className='hero'>
           <StaticImage
@@ -89,3 +79,32 @@ export default function Home() {
     </Layout>
   );
 }
+
+// <section className='about-pages'>
+//   <article>
+//     <h1>What's up, I'm Gilbert</h1>
+//     <p>
+//       web developer and this is where I coding things
+//       <a href=''> web apps about topics</a>
+//     </p>
+
+//     <p className='hero-description'>
+//       web developer and this is where I coding things Lorem ipsum dolor
+//       sit amet consectetur adipisicing elit. Quam, nesciunt!
+//       <a href=''> web apps about topics</a>
+//     </p>
+
+//     <button className='rounded-hover-button'>Hover Me</button>
+
+//     {/* <Link to='/contact' className='btn'>
+//         contact
+//       </Link> */}
+//   </article>
+
+//   <StaticImage
+//     src='../assets/images/me.jpg'
+//     alt='Person Pouring Salt in Bowl'
+//     className='about-imgs'
+//     placeholder='blurred'
+//   />
+// </section>;
